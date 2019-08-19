cb7c6fb22b922215c32f3d47a7a265a5e4ec101a
Lyonghuang <2569871525@qq.com>
2019-08-19 16:38:46 +0800

    增加插入排序、归并排序和堆排序

--- sort.c
+++ sort.c
@@ -3,6 +3,9 @@
 void quickSort(int *a, int start, int end);
 void bubbleSort(int *a, int n);
 void selectionSort(int *a, int n);
+void insertSort(int *a, int n);
+void mergeSort(int *a, int start, int end);
+void heapSort(int *a, int n);
 
 int main() {
 	int n;
@@ -13,7 +16,10 @@ int main() {
 		scanf("%d", &a[i]);
 	}
 	//bubbleSort(a, n);
-	selectionSort(a, n);
+	//selectionSort(a, n);
+	//insertSort(a, n);
+	//mergeSort(a, 0, n-1);
+	heapSort(a, n);
 	for (int i=0; i<n; i++) {
 		printf("%d ", a[i]);
 	}
@@ -68,5 +74,105 @@ void selectionSort(int *a, int n) {
 		temp = a[i];
 		a[i] = a[minIndex];
 		a[minIndex] = temp;
+	}
+}
+
+void insertSort(int *a, int n) {
+	int temp;
+	int p;
+	for (int i = 1; i < n; i++) {
+		temp = a[i];
+		p = i - 1;
+		while (p >= 0 && a[p] > temp) {
+			a[p + 1] = a[p];
+			p--;
+		}
+		a[p + 1] = temp;
+	}
+}
+
+void mergeSort(int *a, int start, int end) {
+	int m = (start + end) / 2;
+	if (end - start > 1) {
+		mergeSort(a, start, m);
+		mergeSort(a, m + 1, end);
+	}
+
+	int p = start;
+	int q = m + 1;
+	int cur = 0;
+	int c[100];
+	while (p <= m && q <= end) {
+		if (a[p] < a[q]) {
+			c[cur++] = a[p];
+			p++;
+		}
+		else {
+			c[cur++] = a[q];
+			q++;
+		}
+	}
+
+	while (p <= m) {
+		c[cur++] = a[p++];
+	}
+	while (q <= end) {
+		c[cur++] = a[q++];
+	}
+
+	for(int i = start; i <= end; i++) {
+		a[i] = c[i-start];
+	}
+}
+
+void heapSort(int *a, int n) {
+	int t[100];
+	int p, q, temp;
+	for (int i = 0; i < n; i++) {
+		t[i] = a[i];
+		p = i;
+		while (p && t[p] < t[(p - 1) / 2]) {
+			temp = t[p];
+			t[p] = t[(p - 1) / 2];
+			t[(p - 1) / 2] = temp;
+			p = (p - 1) / 2;
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		a[i] = t[0];
+		t[0] = t[n - 1 - i];
+		p = 0;
+		int last = n - 1 - i;
+		while (p < last) {
+			if (p * 2 + 2 < last) {//这里分三种情况，当前节点有0，1或2个子节点
+				if (t[p * 2 + 2] < t[p * 2 + 1]) {
+					q = p * 2 + 2;
+				} 
+				else {
+					q = p * 2 + 1;
+				}
+				if (t[p] <= t[q]) {
+					break;
+				}
+				temp = t[p];
+				t[p] = t[q];
+				t[q] = temp;
+				p = q;
+			} 
+			else if (p * 2 + 1 < last) {
+				q = p * 2 + 1;
+				if (t[p] <= t[q]) {
+					break;
+				}
+				temp = t[p];
+				t[p] = t[q];
+				t[q] = temp;
+				p = q;
+			}
+			else {
+				break;
+			}
+		}
 	}
 }
